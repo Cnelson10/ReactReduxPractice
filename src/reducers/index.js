403539c8ce88261c24d833d10644b5111befdae9
Chris <c.lawsonnelson@gmail.com>
2020-08-18 17:35:47 -0400

    redux reducers

---
+++ src/reducers/index.js
@@ -0,0 +1,16 @@
+const songsReducer = () => {
+  return [
+    { title: "Accordian", duration: "1:58" },
+    { title: "Paper Trail$", duration: "3:14" },
+    { title: "One More Round", duration: "3:50" },
+    { title: "Earfquake", duration: "3:05" },
+  ];
+};
+
+const selectedSongReducer = (selectedSong = null, action) => {
+  if ((action.type = "SONG_SELECTED")) {
+    return action.payload;
+  }
+
+  return selectedSong;
+};
